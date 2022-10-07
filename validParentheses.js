b7e4f134127153d79d6a1986085c443c3fcabf64
Shipalan <shipalan678@gmail.com>
2022-10-07 12:24:03 -0600

    Fixed one issue with the solution

--- validParentheses.js
+++ validParentheses.js
@@ -1,19 +1,21 @@
 function validParentheses(parens) {
-    const stack = []
-  
-    for (let i = 0; i < parens.length; i++) {
-      if (parens[i] === '(') {
-        stack.push(parens[i])
-      } else if (parens[i] === ')' && stack[stack.length - 1] === '(') {
-        stack.pop()
-      }
+  const stack = []
+
+  for (let i = 0; i < parens.length; i++) {
+    if (parens[i] === '(') {
+      stack.push(parens[i])
+    } else if (parens[i] === ')' && stack[stack.length - 1] === '(' && stack.length !== 0) {
+      stack.pop()
+    } else {
+      stack.push(parens[i])
     }
-  
-    if (stack.length === 0) {
-      return true
-    }
-    return false
   }
+
+  if (stack.length === 0) {
+    return true
+  }
+  return false
+}
   
   console.log(validParentheses("(())"))
   console.log(validParentheses("("))
